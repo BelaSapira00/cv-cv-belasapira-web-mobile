516ae3fbd8c33f4f3c6cf8d85ada5dcbb6cb0a9f
Bela sapira <belasapira271@gmail.com>
2026-04-03 08:52:35 +0700

    Refactor page loading with fadeOut effect
    Refactor loadPage function to use fadeOut before loading new content.

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
 $(document).ready(function () {
 
   function loadPage(page) {
-    console.log("Load:", page);
-
-    $("#content").hide().load(page, function (response, status) {
-
-      if (status === "error") {
-        $("#content").html("<h3 style='color:red'>File " + page + " tidak ditemukan 😢</h3>");
-      } else {
+    $("#content").fadeOut(200, function () {
+      $("#content").load(page, function () {
         $("#content").fadeIn(200);
-      }
-
+      });
     });
   }
 
-  // 🔥 LOAD HOME PERTAMA
   loadPage("home.html");
 
-  // 🔥 EVENT MENU (ANTI ERROR)
-  $(document).on("click", ".menu", function (e) {
-    e.preventDefault();
-
+  $(".menu").click(function () {
     $(".menu").removeClass("active");
     $(this).addClass("active");
 
     let page = $(this).data("page");
     loadPage(page);
-
-    $("#navMenu").removeClass("show");
-  });
-
-  // 🔥 HAMBURGER
-  $(document).on("click", ".menu-toggle", function () {
-    $("#navMenu").toggleClass("show");
   });
 
 });
