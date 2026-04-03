68a145b2d0d7fd876d8cc5b6434de314b3241f42
Bela sapira <belasapira271@gmail.com>
2026-04-03 08:41:43 +0700

    Refactor page loading and menu interactions
    Updated loadPage function to handle errors and added menu toggle functionality.

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
 
   function loadPage(page) {
-    $("#content").fadeOut(200, function () {
-      $("#content").load(page, function () {
+    console.log("Load:", page);
+
+    $("#content").hide().load("./" + page, function (response, status) {
+
+      if (status === "error") {
+        $("#content").html("<h3 style='color:red'>File tidak ditemukan 😢</h3>");
+      } else {
         $("#content").fadeIn(200);
-      });
+      }
+
     });
   }
 
+  // 🔥 load awal
   loadPage("home.html");
 
-  $(".menu").click(function () {
+  // 🔥 klik menu (ANTI ERROR SPA)
+  $(document).on("click", ".menu", function (e) {
+    e.preventDefault();
+
     $(".menu").removeClass("active");
     $(this).addClass("active");
 
     let page = $(this).data("page");
     loadPage(page);
+
+    $("#navMenu").removeClass("show");
+  });
+
+  // 🔥 hamburger
+  $(document).on("click", ".menu-toggle", function () {
+    $("#navMenu").toggleClass("show");
   });
 
 });
